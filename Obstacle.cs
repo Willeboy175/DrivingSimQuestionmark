49fbdf97dd9190a68e44050e94e73a683a66b56c
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-03-29 10:47:49 +0200

    Obstacle object

--- Obstacle.cs
+++ Obstacle.cs
@@ -8,6 +8,30 @@ namespace DrivingSimQuestionmark
 {
     internal class Obstacle
     {
+        int posX;
+        int posY;
 
+        public Obstacle(int x, int y)
+        {
+            if (x == 1)
+            {
+                posX = 2;
+            }
+            if (x == 2)
+            {
+                posX = 11;
+            }
+            if (x == 3)
+            {
+                posX = 20;
+            }
+
+            posY = y;
+
+            Program.WriteAt("+======+", posX, posY);
+            Program.WriteAt("|######|", posX, posY + 1);
+            Program.WriteAt("|######|", posX, posY + 2);
+            Program.WriteAt("+======+", posX, posY + 3);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -31,6 +31,17 @@ namespace DrivingSimQuestionmark
             origRow = Console.CursorTop;
             origCol = Console.CursorLeft;
 
+            int width = 30;
+            int height = 32;
+
+            Console.SetWindowSize(width, height);
+
+            for (int i = 0; i < height; i += 2)
+            {
+                WriteAt("||        |        |        ||", 0, i);
+                WriteAt("||                          ||", 0, i + 1);
+            }
+
             WriteAt("||        |        |        ||", 0, 0);
             WriteAt("||                          ||", 0, 1);
             WriteAt("||        |        |        ||", 0, 2);
@@ -38,7 +49,11 @@ namespace DrivingSimQuestionmark
 
             Car player = new Car(1, 0);
 
-            
+            Obstacle temp = new Obstacle(1, 11);
+            Obstacle temp2 = new Obstacle(2, 11);
+            Obstacle temp3 = new Obstacle(3, 11);
+
+            /*
             int width = 16;
             int height = 16;
             int what = 5;
@@ -56,7 +71,7 @@ namespace DrivingSimQuestionmark
 
                 Console.ReadLine();
             }
-            
+            */
             Console.ReadLine();
         }
     }
